7992ae26f1dc910b0054b86a0d5f8905b866c057
Sri-Charan-0516 <sricharan824@gmail.com>
2024-04-24 16:04:37 +0530

    24/04/2024

--- src/app/features-modules/admin/project-proposals/project-proposals.component.ts
+++ src/app/features-modules/admin/project-proposals/project-proposals.component.ts
@@ -1,7 +1,7 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { UserService } from 'src/app/Services/user.service';
-import { routes } from 'src/app/core/helpers/routes/routes';
 import { AdminPostProject } from 'src/app/core/models/models';
 
 @Component({
@@ -12,6 +12,9 @@ import { AdminPostProject } from 'src/app/core/models/models';
 export class ProjectProposalsComponent implements OnInit {
 
   adminProject:AdminPostProject[]=[];
+  photoUrl: string | undefined;
+  isLoading: boolean | undefined;
+  photoUrls: any;
   constructor(private userService:UserService,private router:Router){}
 
   ngOnInit(): void {
@@ -21,11 +24,50 @@ export class ProjectProposalsComponent implements OnInit {
   private fetchAllAdminProjects(){
     this.userService.getProjects().subscribe((data)=>{
       this.adminProject=data;
+      const userEmails = this.adminProject.map((adminProject) => adminProject.user.email);
+      // Track number of loaded photos
+      let loadedPhotos = 0;
+      userEmails.forEach((email) => {
+        this.loadPhoto(email, () => {
+          loadedPhotos++;
+
+          // Check if all photos are loaded
+          if (loadedPhotos === userEmails.length) {
+            this.isLoading = false;
+          }
+        });
+      });
       console.log(this.adminProject);
     })
   }
 
   viewProposal(id:number){
     this.router.navigate(['/admin/view-proposals',id])
+  }
+
+  loadPhoto(email: string, callback: () => void): void {
+    this.userService.getPhoto(email).subscribe(
+      (data: Blob) => {
+        const reader = new FileReader();
+        reader.onload = () => {
+          if (!this.photoUrls) {
+            this.photoUrls = {};
+          }
+          // Assign the loaded photo URL to the respective email
+          this.photoUrls[email] = reader.result as string;
+          console.log('Photo URL for', email, ':', this.photoUrls[email]); // Log photo URL
+          callback(); // Invoke callback when photo is loaded
+        };
+        reader.onerror = (error) => {
+          console.error('Error reading photo data:', error);
+          this.isLoading = false; // Set loading to false on error
+        };
+        reader.readAsDataURL(data);
+      },
+      (error: any) => {
+        console.error('Error loading photo:', error);
+        this.isLoading = false; // Set loading to false on error
+      }
+    );
   }
 }
--- src/app/features-modules/freelancer/project/ongoing-projects/ongoing-projects.component.ts
+++ src/app/features-modules/freelancer/project/ongoing-projects/ongoing-projects.component.ts
@@ -1,11 +1,38 @@
-import { Component } from '@angular/core';
+/* eslint-disable @typescript-eslint/no-explicit-any */
+/* eslint-disable @typescript-eslint/no-empty-function */
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { UserService } from 'src/app/Services/user.service';
 import { routes } from 'src/app/core/helpers/routes/routes';
+import { AdminPostProject } from 'src/app/core/models/models';
+import { AuthService } from 'src/app/core/services/auth/auth.service';
 
 @Component({
   selector: 'app-ongoing-projects',
   templateUrl: './ongoing-projects.component.html',
   styleUrls: ['./ongoing-projects.component.scss']
 })
-export class OngoingProjectsComponent {
+export class OngoingProjectsComponent implements OnInit{
   public routes = routes;
+  projects:AdminPostProject[]=[];
+  email!:string;
+  constructor(private userService:UserService,private auth:AuthService,private router:Router){}
+  
+  ngOnInit(): void {
+    this.email=this.auth.getEmail();
+    this.loadOngoingProjects(this.email);
+  }
+
+  private loadOngoingProjects(email:string){
+    this.userService.freelancerOnGoingProjects(email).subscribe((data:any)=>{
+      this.projects=data;
+    },error=>{
+      console.log("No Projects Found..!!!",error)
+    })
+  }
+
+  fetchProject(id:number){
+    console.log(id);
+    this.router.navigate(['/freelancer/view-project-detail',id])
+  }
 }
