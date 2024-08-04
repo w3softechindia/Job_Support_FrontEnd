b426984cda51db8c2be937986b4deff034539c05
Sri-Charan-0516 <147786405+Sri-Charan-0516@users.noreply.github.com>
2024-08-04 21:46:12 +0530

    Charan (#55)
    * New Code changes
    
    * 23/02/2024
    
    * 26/02/2024
    
    * 27/02/2024
    
    * 28/02/2024
    
    * 29/02/2024
    
    * 1/03/2024
    
    * 01/03/2024
    
    * 01/03/2024
    
    * 03/03/2024
    
    * 04/03/2024
    
    * 04/03/2024
    
    * 05/03/2024
    
    * 05/03/2024
    
    * 06/03/2024
    
    * 07/03/2024
    
    * 13/03/2024
    
    * 14/03/2024
    
    * 15/03/2024
    
    * 18/03/2024
    
    * 18/03/2024
    
    * 19/03/2024
    
    * 20/03/2024
    
    * 22/03/2024
    
    * 26/03/2024
    
    * 01/04/2024
    
    * 01/04/2024
    
    * 03/04/2024
    
    * 05/04/2024
    
    * 05/04/2024
    
    * 19/04/2024
    
    * 22/04/2024
    
    * 24/04/2024
    
    * 26/04/2024
    
    * 14/05/2024
    
    * 23/05/2024
    
    * 07/06/2024
    
    * 11/07/2024
    
    * 04/08/2024
    
    * 04/08/2024
    
    * 04/08/2024
    
    * 04/08/2024
    
    ---------
    
    Co-authored-by: w3softechindia <160119484+w3softechindia@users.noreply.github.com>

---
+++ server.js
@@ -0,0 +1,11 @@
+let express = require('express');
+
+let app = express();
+
+app.use(express.static(__dirname+'/dist/kofejob-angular'));
+
+app.get('/*',(req,resp)=>{
+    resp.sendFile(__dirname+'/dist/kofejob-angular/index.html');
+});
+
+app.listen(process.env.PORT || 8080);
